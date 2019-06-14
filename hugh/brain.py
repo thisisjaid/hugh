aa9866387d26cb93a81a4b51497406a846623e8b
Alex Enachioaie <alex@altmetric.com>
2019-06-14 15:26:51 +0100

    Add hugh's markov brain

---
+++ hugh/brain.py
@@ -0,0 +1,33 @@
+import re
+from .cobe.brain import Brain
+from .config import *
+
+brain_path = f'data/brain/{BOT_BRAIN_ID}'
+os.makedirs('data/brain/', exist_ok=True)
+
+hugh = Brain(brain_path)
+
+def filter(message):
+    message = re.sub('\<@[A-Z0-9a-z]{9}\>', '', message) # remove mentions
+    message = re.sub('\s{2,}', ' ', message) #remove double spaces
+    message = re.sub('\<[^\<]+\>', '', message) #remove links
+    message = message.strip() # remove unneeded spaces
+    valid = False
+    if len(message) > 5 and len(message) <= 100:
+        valid = True
+    return valid,message
+
+def learn(message):
+    global hugh
+    valid, message = filter(message)
+    if not valid:
+        return
+    hugh.learn(message)
+
+def backtalk(message):
+    global hugh
+    response = hugh.reply(message, loop_ms=1000, max_len=100)
+    valid, response = filter(response)
+    if not valid:
+        return None
+    return response
