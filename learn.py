9f29432c97f99dd036faf20e3a8ced2f5574f1dc
Alex Enachioaie <alex@altmetric.com>
2019-06-14 15:36:54 +0100

    Keep this as a file reader

--- learn.py
+++ learn.py
@@ -10,7 +10,6 @@ files = [f for f in os.listdir('to_import')]
 for i in files:
     with open(f'to_import/{i}', 'r') as f:
         content = json.loads(f.read())
-    messages = [message['text'] for message in content if message['type'] == 'message' and 'subtype' not in message]
     for message in messages:
         print('learning...')
         markov.learn(message)
